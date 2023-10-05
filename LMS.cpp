e298d0df22df4e4a7fdc26843a059069272c78e3
MishanPoudel <poudelmishan2@gmail.com>
2023-10-05 23:01:32 +0545

    added first option

---
+++ LMS.cpp
@@ -0,0 +1,13 @@
+#include<istream>
+using namespace std;
+
+int main(){
+    cout << "********** MISERY LIBRARY MANAGEMENT SYSTEM **********\n";
+    cout << ">>Choose any option to get started with:"
+
+
+}
+
+int newStudent(){
+    
+}
