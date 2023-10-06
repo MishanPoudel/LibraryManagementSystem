482ae4878cb803e6a0700559b5b784fadf7b28a3
MishanPoudel <poudelmishan2@gmail.com>
2023-10-06 22:33:35 +0545

    added new book option

--- LMS.cpp
+++ LMS.cpp
@@ -54,10 +54,59 @@ int newStudent()
     }
 }
 
+void newBook()
+{
+    ofstream books("books.txt", ios::app);
+    if (books.is_open())
+    {
+        int id, price, year;
+        string name, publisher;
+        cout << "How many books to add?:";
+        cin >> count;
+        for (int i = 1; i <= count; i++)
+        {
+            cout << "Book ID: ";
+            cin >> id;
+
+            cout << "Book Name: ";
+            cin.ignore();
+            getline(cin, name);
+
+            cout << "Publisher: ";
+            getline(cin, publisher);
+
+            cout << "Price: ";
+            cin >> price;
+
+            cout << "Publication Year: ";
+            cin >> year;
+
+            books << endl
+                  << "Book ID: " << id << endl;
+            books << "Name: " << name << endl;
+            books << "Publisher: " << publisher << endl;
+            books << "Price: " << price << endl;
+            books << "Year: " << year << endl;
+
+            cout << "Book information added successfully!" << endl;
+        }
+    }
+    else
+    {
+        cout << "Error opening the file for writing." << endl;
+    }
+
+    books.close();
+}
+
 int main()
 {
     cout << "********** MISERY LIBRARY MANAGEMENT SYSTEM **********\n";
     cout << ">>Choose any option to get started with:\n";
-    cout << "1. New Student\n";
-    newStudent();
+    cout << "1. New Student\n"
+         << "2. New Book\n"
+         << "2. \n"
+         << "2. New Book\n";
+    // newStudent();
+    newBook();
 }
